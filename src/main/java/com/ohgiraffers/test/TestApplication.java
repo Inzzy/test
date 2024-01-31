e1caddedc5eecb79fc04ae8d3ef9631c57913f83
pyunghun <cvaria223179@gmail.com>
2024-01-31 14:31:12 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    // 수업목표 누구신지요?
+    
+    // 여기는 주석입니다.
+    /* 여기는
+     * 여러 줄
+     * 주석입니다.
+     *  */
+
+    /*
+     * ctrl - space : 자공완성 기능
+     *  */
+
+    // 히히히힛
+    // 히히히힛
+    // 히히히힛
+    // 히히히힛
+    // 히히히힛
+
+    public static void main(String[] args) {
+
+        System.out.println("저도 개발자입니다.");
+
+
+    }
+}
